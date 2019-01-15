35ad5877bf89643e23efea7a090da08f8c90cb6b
Amran Ahmad <amran-ahmad@live.com>
2019-01-15 15:49:09 +0100

    k8s artifacts

---
+++ gasnltimeseries/src/main/java/com/vattenfall/gasnltimeseries/GasNLTimeseriesApplication.java
@@ -0,0 +1,14 @@
+package com.vattenfall.gasnltimeseries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasNLTimeseriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GasNLTimeseriesApplication.class, args);
+    }
+
+}
+
